65d39f26661977b3ce5cf0283e5516f41babec31
debaduttakore <139857436+debaduttakore@users.noreply.github.com>
2023-10-20 22:38:49 +0530

    add root route for health check

--- app/index.js
+++ app/index.js
@@ -1,19 +1,19 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
-const cors = require('cors');
-const apiRouter = require('../routes');
+const cors = require("cors");
+const apiRouter = require("../routes");
 const sessionMiddleware = require("../middlewares/sessionMiddleWare");
-app.use(cors({ origin: true,credentials: true }))
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
-app.use(cookieParser('2@]>+k70fX8S:74Ou0Dz7:XPvk'))
+app.use(cookieParser("2@]>+k70fX8S:74Ou0Dz7:XPvk"));
 app.use(sessionMiddleware);
-app.use('/',apiRouter);
-app.use('/api',apiRouter);
-app.use('/',(req,res)=>{
-    res.send('<h1>kore.ai</h1>');
-})
+app.use("/", apiRouter);
+app.use("/api", apiRouter);
+app.use("/", (req, res) => {
+    res.send("<h1>kore.ai</h1>");
+});
 
 module.exports = app;
