0e72bd60d2ebfa269bc7fca62b60d9297dc87b6d
debaduttakore <139857436+debaduttakore@users.noreply.github.com>
2023-10-20 23:39:00 +0530

    shift route to root

--- app/index.js
+++ app/index.js
@@ -4,8 +4,12 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 const cors = require("cors");
-const apiRouter = require("../routes");
 const sessionMiddleware = require("../middlewares/sessionMiddleWare");
+const {loginWithSession, login, deleteUserSession} = require('../routes/auth');
+const { protectRoute } = require('../middlewares/protecteRoute');
+const { addUserData, updateUserData, deleteUserData, getAllUserData } = require('../routes/user');
+const { addUsecaseData, updateUsecaseData, deleteUsecaseData, getAllUsecaseData } = require('../routes/usecases');
+
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(cookieParser("2@]>+k70fX8S:74Ou0Dz7:XPvk"));
@@ -13,12 +17,20 @@ app.use(sessionMiddleware);
 app.get('/',(req,res)=>{
     res.send('<h1> Kore.ai </h1>');
 });
-app.get('/hi',(req,res)=>{
-    res.send('<h1> Kore.ai </h1>');
-});
-app.get('/hi/hello',(req,res)=>{
-    res.send('<h1> Kore.ai </h1>');
-});
-app.use("/api", apiRouter);
+// routes for authentication
+app.post('/auth/login',login);
+app.get('/auth/session',protectRoute, loginWithSession);
+app.delete('/auth/logout', protectRoute,deleteUserSession);
+
+// routes for users
+app.post('/user/add',protectRoute , addUserData);
+app.delete('/user/delete', protectRoute, deleteUserData);
+app.post('/user/all', protectRoute, getAllUserData);
+
+// routes for usecases
+app.post('/usecase/add',protectRoute, addUsecaseData);
+app.put('/usecase/update',protectRoute, updateUsecaseData);
+app.delete('/usecase/delete',protectRoute, deleteUsecaseData);
+app.post('/usecase/all', protectRoute, getAllUsecaseData);
 
 module.exports = app;
