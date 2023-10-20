893d8ebd0b20f16fade3b664a3135e10ad6fd493
debaduttakore <139857436+debaduttakore@users.noreply.github.com>
2023-10-20 22:09:32 +0530

    modify cors

--- app/index.js
+++ app/index.js
@@ -1,13 +1,12 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 const express = require('express');
-const serverless = require('serverless-http');
 const cookieParser = require('cookie-parser');
 const app = express();
 const cors = require('cors');
 const apiRouter = require('../routes');
 const sessionMiddleware = require("../middlewares/sessionMiddleWare");
-app.use(cors({ origin: true, credentials: true }))
+app.use(cors({ origin: "*", credentials: true }))
 app.use(express.json());
 app.use(cookieParser('2@]>+k70fX8S:74Ou0Dz7:XPvk'))
 app.use(sessionMiddleware);
