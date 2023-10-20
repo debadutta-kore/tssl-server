787ab003af37cfa74bfef2032424e675f97e7dcc
debaduttakore <139857436+debaduttakore@users.noreply.github.com>
2023-10-20 23:46:43 +0530

    change

--- app/index.js
+++ app/index.js
@@ -4,12 +4,14 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 const cors = require("cors");
+const serverless = require('serverless-http');
 const apiRouter = require("../routes");
 const sessionMiddleware = require("../middlewares/sessionMiddleWare");
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(cookieParser("2@]>+k70fX8S:74Ou0Dz7:XPvk"));
 app.use(sessionMiddleware);
+
 app.get('/',(req,res)=>{
     res.send('<h1> Kore.ai </h1>');
 });
