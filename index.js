e14094e0a86d8f2484e6e908ff15489831059d8b
debaduttakore <139857436+debaduttakore@users.noreply.github.com>
2023-10-22 12:15:10 +0530

    add own cors middleware

--- index.js
+++ index.js
@@ -3,25 +3,11 @@ require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
-const cors = require("cors");
 const apiRouter = require("./routes");
 const sessionMiddleware = require("./middlewares/sessionMiddleWare");
+const cors = require("./middlewares/cors");
 
-const allowedOrigins = ['https://delicate-marigold-3bd910.netlify.app','https://tssl-client-dcml2ei5t-devs-projects-4804e324.vercel.app'];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors);
 app.use(express.json());
 app.use(cookieParser("2@]>+k70fX8S:74Ou0Dz7:XPvk"));
 app.use(sessionMiddleware);
