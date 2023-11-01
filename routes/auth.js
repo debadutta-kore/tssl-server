d05858ffd63d577336f82aed022e23c94db14352
debaduttakore <139857436+debaduttakore@users.noreply.github.com>
2023-11-01 14:06:22 +0530

    production test

--- routes/auth.js
+++ routes/auth.js
@@ -36,7 +36,7 @@ module.exports.login = (req, res, next) => {
                 const cookieSettings = {
                   signed: true,
                   httpOnly: true,
-                  domain: new URL(req.get('origin')).host,
+                  domain: 'delicate-marigold-3bd910.netlify.app',
                   expires: new Date(Date.now() + 30 * 60 * 1000),
                 };
                 // if (req.protocol === "https") {
