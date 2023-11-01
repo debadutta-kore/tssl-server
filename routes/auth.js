74fcbe4e6fd4f30697fec87cd03e596bc1884e16
debaduttakore <139857436+debaduttakore@users.noreply.github.com>
2023-11-01 14:02:36 +0530

    production test

--- routes/auth.js
+++ routes/auth.js
@@ -36,12 +36,13 @@ module.exports.login = (req, res, next) => {
                 const cookieSettings = {
                   signed: true,
                   httpOnly: true,
+                  domain: new URL(req.get('origin')).host,
                   expires: new Date(Date.now() + 30 * 60 * 1000),
                 };
-                if (req.protocol === "https") {
-                  cookieSettings["secure"] = true;
-                  cookieSettings["sameSite"] = "None";
-                }
+                // if (req.protocol === "https") {
+                //   cookieSettings["secure"] = true;
+                //   cookieSettings["sameSite"] = "None";
+                // }
                 res.cookie("sessionId", req.sessionId, cookieSettings);
                 res.status(200).send({
                   message: "Successfully logged in",
