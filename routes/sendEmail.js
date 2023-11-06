df1e45c3e3fb3155567d81448189942b6bc695bc
debaduttakore <139857436+debaduttakore@users.noreply.github.com>
2023-11-06 23:27:26 +0530

    update send mail and add help and support email template

--- routes/sendEmail.js
+++ routes/sendEmail.js
@@ -1,26 +1,36 @@
 const mail = require("@sendgrid/mail");
+const { getRowById } = require("../db");
+const ejs = require('ejs');
+const path = require("path");
+//`From: ${_res.data.email}\nName: ${_res.data.name}\n${req.body.description}`,
 module.exports = (req, res, next) => {
-  mail
-    .send({
-      from: "travelassistdev@kore.com",
-      to: "foodassistdev@kore.com",
-      subject: `${req.body.queryType}-${req.body.subject}`,
-      text: req.body.description,
-      attachments: req.files.map((file) => ({
-        content: file.content,
-        filename: file.name,
-        type: file.type,
-        disposition: "attachment",
-      })),
-    })
-    .then(() => {
-      res.status(200).send({message:'email send successfully'});
-    })
-    .catch((error) => {
-      if (error.response) {
-        res.status(200).send(error.response.body);
-      } else {
-        next(error);
-      }
-    });
+  getRowById('user',
+    req.sessionData.role === "user"
+      ? req.sessionData.userId
+      : req.sessionData.adminId
+  ).then(async (_res)=>{
+    if(_res && _res.data) {
+      const htmlStr = await ejs.renderFile(path.join(__dirname,'../templates/helpSupport.ejs'),{
+        from: _res.data.email,
+        name: _res.data.name,
+        body: req.body.description
+      });
+      mail
+      .send({
+        from: "travelassistdev@kore.com",
+        to: [req.body.to, "foodassistdev@kore.com"],
+        subject: `${req.body.queryType}-${req.body.subject}`,
+        html: htmlStr,
+        attachments: req.files.map((file) => ({
+          content: file.content,
+          filename: file.name,
+          type: file.type,
+          disposition: "attachment",
+        })),
+      })
+      .then(() => {
+        res.status(200).send({ message: "email send successfully" });
+      })
+    }
+  }).catch(next);
 };
