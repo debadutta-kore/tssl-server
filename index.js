da478eab67c0d0f229291ece096fb235df05fd73
debaduttakore <139857436+debaduttakore@users.noreply.github.com>
2023-12-05 12:33:21 +0530

    run application on multiple instance of server

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const { app } = require("./app");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`app is listening on port ${PORT}!`));
+if (cluster.isPrimary) {
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  app.listen(PORT, () => console.log(`app is listening on port ${PORT}!`));
+  console.log(`Worker ${process.pid} started`);
+}
