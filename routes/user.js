732e23fda0ee6d566f9b533ac53bcd35df79c5b3
Debadutta Panda <debadutta.panda@kore.com>
2024-01-03 18:36:46 +0530

    resolve path bug

--- routes/user.js
+++ routes/user.js
@@ -25,7 +25,7 @@ module.exports.addUser = (req, res, next) => {
                   url: url.format({
                     protocol: req.protocol,
                     host: req.get("host"),
-                    pathname:'/'+process.env.rootPath+'/'
+                    pathname: process.env.rootPath+'/'
                   }),
                 });
                 res.status(201).send({
