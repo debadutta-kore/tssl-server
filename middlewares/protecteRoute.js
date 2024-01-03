c78dc10d199606d3abfc8009c143ed0b73215ef1
Debadutta Panda <debadutta.panda@kore.com>
2024-01-03 18:09:52 +0530

    commit client and server changes

--- middlewares/protecteRoute.js
+++ middlewares/protecteRoute.js
@@ -1,14 +1,11 @@
-const { getRows } = require("../db");
+const { getRowById } = require("../db");
 
 module.exports.protectRoute = function (req,res,next) {
     if(req.sessionData) {
         if(req.sessionData.role === 'user') {
-            // if the user account is not disable
-            getRows("user", {
-              enable: 1,
-              _id: req.sessionData.userId,
-            }).then((_res) => {
-              if (_res.data && _res.data.records.length > 0) {
+            // Check if the user account is not disabled
+            getRowById("user",req.sessionData.userId).then((_res) => {
+              if (_res.data && _res.data.enable === 1) {
                 next();
               } else {
                 res
